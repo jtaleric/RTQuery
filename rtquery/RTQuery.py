bae4c0d2ce2626bc7d0fd4ceca4eee3061c09471
Joe Talerico <jtaleric@redhat.com>
2017-10-09 15:58:07 -0400

    Pushing comments to existing RT Tickets
    Added the ability to push updates to RT tickets.

--- rtquery/RTQuery.py
+++ rtquery/RTQuery.py
@@ -22,7 +22,8 @@ class RTQuery(object):
 
     def __init__(self, config):
         self.CONFIG = config
-        self.es = Elastic.Elastic(self.CONFIG['elastichost'],self.CONFIG['elasticport'])
+        self.es = Elastic.Elastic(
+            self.CONFIG['elastichost'], self.CONFIG['elasticport'])
 
     def _query(self, url, querystring=None):
         krb = KerberosTicket.KerberosTicket(self.CONFIG['domain'])
@@ -32,6 +33,13 @@ class RTQuery(object):
         else:
             req = requests.get(url, headers=headers,
                                params=querystring, verify=False)
+        return req
+
+    def _push(self, url, payload):
+        krb = KerberosTicket.KerberosTicket(self.CONFIG['domain'])
+        headers = {"Authorization": krb.auth_header}
+        req = requests.get(url, headers=headers,
+                           params=payload, data=payload, verify=False)
         return req
 
     def rt_search(self, querystring):
@@ -44,13 +52,18 @@ class RTQuery(object):
                 lab_requests[str(values[0].strip())] = str(values[1].strip())
         return lab_requests
 
-    def es_insert_rt(self, rt_id, request) :
-        if len(self.es_search_rt(rt_id)) is 0 :
-           return self.es.index(request,self.CONFIG['index'])
+    def rt_history_comment(self, ticket, message):
+        url = "{}/{}/comment".format(self.CONFIG['urls']['ticket'], ticket)
+        payload = {'content': {"Action: comment\nText: {}".format(message)}}
+        return self._push(url, payload).text
+
+    def es_insert_rt(self, rt_id, request):
+        if len(self.es_search_rt(rt_id)) is 0:
+            return self.es.index(request, self.CONFIG['index'])
 
     def es_search_rt(self, rt_id):
         query = {"query": {"match": {"ticket": rt_id}}}
-        req = self.es.search(self.CONFIG['index'],query)
+        req = self.es.search(self.CONFIG['index'], query)
         return req['hits']['hits']
 
     def rt_history(self, rt_id):
@@ -77,8 +90,9 @@ class RTQuery(object):
         request = {}
         for line in r.text.split('\n'):
             if ':' in line:
-                values = line.split(":",2)
-                if len(values) > 2:
+                values = line.split(":", 1)
+                if values[1].count(':') is 1:
+                    values = values[1].split(":", 1)
                     request[str(values[0].encode('utf-8').strip())
                             ] = str(values[1].encode('utf-8').strip())
                 else:
@@ -87,29 +101,45 @@ class RTQuery(object):
         return request
 
     def rt_es_mapper(self, request):
+        products = {'Openstack': 'OpenStack',
+                    'RHOP': 'OpenStack',
+                    'OpenStack RHOSP-11': 'OpenStack',
+                    'Red Hat OpenStack Newton (10)': 'OpenStack',
+                    'openstack': 'OpenStack',
+                    'OSP 11': 'OpenStack',
+                    'Openstack, TripleO': 'OpenStack',
+                    'Red Hat OpenStack Platform': 'Openstack',
+                    'RedHat Openstack': 'OpenStack',
+                    'rhel osp': 'OpenStack',
+                    'Red Hat OpenStack Ocata (11)': 'OpenStack'}
         mapper = {'timestamp': request['Created'],
-               'author': request['Creator'],
-               'description': request['Description'],
-               'ticket': request['Ticket'],
-               'id': request['id']}
-        if 'Use Case' in request :
+                  'author': request['Creator'],
+                  'description': request['Description'],
+                  'ticket': request['Ticket'],
+                  'id': request['id']}
+        if 'Use Case' in request:
             mapper['use'] = request['Use Case'],
-        if 'are you targeting to improve?' in request :
-            mapper['use'] = request['are you targeting to improve?'],
-        else :
+        if 'are you targeting to improve?' in request:
+            mapper['use'] = request['are you targeting to improve?']
+        else:
             mapper['use'] = 'unknown'
-        if 'Red Hat product(s) being tested' in request: 
-               mapper['product'] =  request['Red Hat product(s) being tested'],
+        if 'Red Hat product(s) being tested' in request:
+            mapper['product'] = request['Red Hat product(s) being tested']
         elif 'Performance Red Hat product(s) being tested' in request:
-               mapper['product'] =  request['Performance Red Hat product(s) being tested']
+            mapper['product'] = request[
+                'Performance Red Hat product(s) being tested']
         else:
-               mapper['product'] = 'unknown' 
-        if 'Number of nodes being requested' in request :
-               mapper['node_count'] = request['Number of nodes being requested'].split(" ")[0],
+            mapper['product'] = 'unknown'
+        if 'Number of nodes being requested' in request:
+            mapper['node_count'] = request['Number of nodes being requested'].split(" ")[
+                0]
         elif 'being requested' in request:
-               mapper['node_count'] = request['being requested'].split(" ")[0],
-        elif 'Platform Number of nodes being requested' in request :
-               mapper['node_count'] = request['Platform Number of nodes being requested'].split(" ")[0],
+            mapper['node_count'] = request['being requested'].split(" ")[0]
+        elif 'Platform Number of nodes being requested' in request:
+            mapper['node_count'] = request[
+                'Platform Number of nodes being requested'].split(" ")[0]
         else:
-               mapper['node_count'] = 0,
+            mapper['node_count'] = 0,
+        if mapper['product'] in products:
+            mapper['product'] = products[mapper['product']]
         return mapper
